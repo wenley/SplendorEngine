d43fe8006852cb38505c6c90dbf589723be376ec
Wenley Tong <wenley@squareup.com>
2017-05-11 00:54:29 -0400

    Add notes to direct further development.
    See all missing parts of the pipeline.

---
+++ engine.ml
@@ -0,0 +1,21 @@
+
+(**
+ * Steps:
+ * - Start the game
+ *   - Get player count
+ *   - Initialize board data (start.ml)
+ *   - Get player names
+ *   - Make game object
+ * - Game loop, for each player
+ *   - Display state of the world
+ *   - Read action
+ *   - Validate action against board + player state
+ *     - Translate from relative indexes to concrete data (cards)
+ *     - Read another action if invalid
+ *   - Perform action
+ *     - Update all relevant data structures
+ *   - Check for cleanup
+ *     - Claim nobles
+ *     - Put back tokens
+ * - After each full loop, check for end of game
+ *)
