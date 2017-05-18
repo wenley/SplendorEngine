7b9759b9745e0e7be1dda2d92f6073d099704d01
Wenley Tong <wenley@squareup.com>
2017-05-17 18:43:34 -0700

    Fill in more pieces.

--- validation.ml
+++ validation.ml
@@ -1,19 +1,31 @@
 
 let enough_tokens (required:Data.tokens) (tokens:Data.tokens) : bool =
   let tokens_has (token:Data.token) (count:int) : bool =
-    let tokens_value = try Some (Data.TokenMap.find token tokens) with Not_found -> None
-    in match count, tokens_value with
-    | 0, _ -> true
-    | _, None -> false
-    | needed, Some have -> needed <= have
+    let tokens_value = try Data.TokenMap.find token tokens with Not_found -> 0 in
+    count <= tokens_value
   in
   Data.TokenMap.for_all tokens_has required
+
+let tokens_cover (required:Data.cost) (tokens:Data.tokens) : bool =
+  let tokens_short (color:Data.color) (count:int) (sum:int) : int =
+    let tokens_value = try Data.TokenMap.find (Normal color) tokens with Not_found -> 0
+    in if count <= tokens_value then sum
+    else (sum + count - tokens_value)
+  in
+  let golds_needed = Data.ColorMap.fold tokens_short required 0 in
+  let golds_have = try Data.TokenMap.find Gold tokens with Not_found -> 0 in
+  golds_needed <= golds_have
 
 let board_has (tokens:Data.tokens) (board:Board.board) : bool =
   enough_tokens tokens board.Board.tokens
 
+let apply_discount (discount:Data.cost) (cost:Data.cost) : Data.cost =
+  cost
+
 let can_purchase (player:Player.player) (card:Data.card) : bool =
-  true
+  let discount = Player.total_discount player in
+  let discounted_cost = apply_discount discount card.Data.cost in
+  tokens_cover discounted_cost player.Player.tokens
 
 let valid action player board =
   match action with
