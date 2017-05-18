9ae61f6e6a92e5d731942718375fa2f4192b9579
Wenley Tong <wenley@squareup.com>
2017-05-17 18:14:12 -0700

    Add first actual test case.

--- test/validation_test.ml
+++ test/validation_test.ml
@@ -3,7 +3,57 @@ open Player;;
 open Board;;
 open Validation;;
 
-let buy_test_case (player_tokens:tokens) (card_cost:cost) (expected:bool) : unit =
+let empty_deck = { deck=[]; revealed=[] }
+;;
+
+let three_test_case (c1:color) (c2:color) (c3:color) (board_tokens:tokens) (expected:bool) : unit =
+  let player = {
+    name="";
+    tokens=TokenMap.empty;
+    nobles=[];
+    cards=[];
+    reserved=[];
+  } in
+  let board = {
+    one=empty_deck;
+    two=empty_deck;
+    three=empty_deck;
+    nobles=[];
+    tokens=board_tokens;
+  } in
+  let message =
+    Printf.printf "%s%s%s vs %s on board %s\n"
+    (string_of_color c1)
+    (string_of_color c2)
+    (string_of_color c3)
+    (verbose_string_of_tokens board_tokens)
+  in
+  let action : Action.action = Action.Three(c1, c2, c3) in
+  let result = valid action player board in
+  match result = expected with
+  | true -> message "passed"
+  | false -> message "failed"
+;;
+
+let three_test_cases =
+  (Red, Red, Red, TokenMap.empty, false)
+  :: (Red, Red, Blue, TokenMap.empty, false)
+  :: (Red, Green, Blue, TokenMap.empty, false)
+  :: (Red, Green, Blue, TokenMap.empty
+                        |> TokenMap.add (Normal Red) 1
+                        |> TokenMap.add (Normal Green) 1
+                        |> TokenMap.add (Normal Blue) 1, true)
+  :: (Red, Green, Blue, TokenMap.empty
+                        |> TokenMap.add (Gold) 1
+                        |> TokenMap.add (Normal Green) 1
+                        |> TokenMap.add (Normal Blue) 1, false)
+  :: []
+in let run_test (c1, c2, c3, board_tokens, expected) : unit =
+  three_test_case c1 c2 c3 board_tokens expected
+in List.iter run_test three_test_cases
+;;
+
+let can_purchase_test_case (player_tokens:tokens) (card_cost:cost) (expected:bool) : unit =
   let player =
     {
       name="test";
@@ -11,16 +61,16 @@ let buy_test_case (player_tokens:tokens) (card_cost:cost) (expected:bool) : unit
       nobles=[];
       cards=[];
       reserved=[];
-    }
-  in let board =
-    let card = { score=0; color=Blue; cost=card_cost } in
-    let empty_deck = { deck=[]; revealed=[] } in
-    {
-      one={ deck=[]; revealed=card::[] };
-      two=empty_deck;
-      three=empty_deck;
-      nobles=[];
-      tokens=TokenMap.empty;
-    }
-  in ()
+    } in
+  let card = { score=0; color=Blue; cost=card_cost } in
+  let message =
+    Printf.printf "Player tokens %s vs. Card cost %s %s\n"
+    (verbose_string_of_tokens player_tokens)
+    (verbose_string_of_cost card_cost)
+  in
+  let result = can_purchase player card in
+  match result = expected with
+  | true -> message "passed"
+  | false -> message "failed"
+
 
--- validation.mli
+++ validation.mli
@@ -1,2 +1,4 @@
 
+val can_purchase : Player.player -> Data.card -> bool
+
 val valid : Action.action -> Player.player -> Board.board -> bool
