7557ecf6cf22a475166fd0a276a8991dbc29078a
Wenley Tong <wenley@squareup.com>
2017-05-18 17:32:44 -0700

    Make it easier to specify map collections.

--- test/validation_test.ml
+++ test/validation_test.ml
@@ -6,6 +6,19 @@ open Validation;;
 let empty_deck = { deck=[]; revealed=[] }
 ;;
 
+let rec token_map_of (tokens:token list) : tokens =
+  match tokens with
+  | [] -> TokenMap.empty
+  | hd :: tl -> token_map_of tl |> TokenCounter.increment hd
+;;
+
+let rec cost_of (colors:color list) : cost =
+  match colors with
+  | [] -> ColorMap.empty
+  | hd :: tl -> cost_of tl |> ColorCounter.increment hd
+;;
+
+
 let run_three_test (c1:color) (c2:color) (c3:color) (board_tokens:tokens) (expected:bool) : unit =
   let player = {
     name="";
@@ -39,14 +52,8 @@ let three_test_cases =
   (Red, Red, Red, TokenMap.empty, false)
   :: (Red, Red, Blue, TokenMap.empty, false)
   :: (Red, Green, Blue, TokenMap.empty, false)
-  :: (Red, Green, Blue, TokenMap.empty
-                        |> TokenMap.add (Normal Red) 1
-                        |> TokenMap.add (Normal Green) 1
-                        |> TokenMap.add (Normal Blue) 1, true)
-  :: (Red, Green, Blue, TokenMap.empty
-                        |> TokenMap.add (Gold) 1
-                        |> TokenMap.add (Normal Green) 1
-                        |> TokenMap.add (Normal Blue) 1, false)
+  :: (Red, Green, Blue, token_map_of (Normal Red :: Normal Green :: Normal Blue :: []), true)
+  :: (Red, Green, Blue, token_map_of (Gold :: Normal Green :: Normal Blue :: []), false)
   :: []
 in let run_test (c1, c2, c3, board_tokens, expected) : unit =
   run_three_test c1 c2 c3 board_tokens expected
@@ -75,8 +82,8 @@ let run_can_purchase_test (player_tokens:tokens) (card_cost:cost) (expected:bool
 ;;
 
 let can_purchase_test_cases =
-  (TokenMap.empty, ColorMap.empty, true)
-  :: (TokenMap.empty, ColorMap.empty |> ColorMap.add Red 1, false)
+  (token_map_of [], cost_of [], true)
+  :: (token_map_of [], cost_of (Red :: []), false)
   :: []
 in let run_test (player_tokens, card_cost, expected) : unit =
   run_can_purchase_test player_tokens card_cost expected
