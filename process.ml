625973a50535a4b89382e6c8ac9388e7b0af5320
Wenley Tong <wenley.tong@gmail.com>
2017-06-06 10:15:38 -0400

    Implement Two case for process_action.

--- process.ml
+++ process.ml
@@ -16,9 +16,26 @@ let process_three c1 c2 c3 player board =
   in
   (new_player, new_board)
 
+let process_two color player board =
+  let new_player =
+    let new_tokens = player.Player.tokens |>
+    Data.TokenCounter.increment (Data.Normal color) |>
+    Data.TokenCounter.increment (Data.Normal color)
+    in { player with Player.tokens=new_tokens }
+  in
+  let new_board = 
+    let new_tokens = board.Board.tokens |>
+    Data.TokenCounter.decrement (Data.Normal color) |>
+    Data.TokenCounter.decrement (Data.Normal color)
+    in { board with Board.tokens=new_tokens }
+  in
+  (new_player, new_board)
+
 let process_action action player board =
   match action with
   | Action.Three (c1, c2, c3) ->
       process_three c1 c2 c3 player board
+  | Action.Two (color) ->
+      process_two color player board
   | _ -> (player, board)
 
--- test/process_test.ml
+++ test/process_test.ml
@@ -47,3 +47,48 @@ let after_board = after_player in
 run_process_three_test Red Green Blue before_player after_player before_board
 after_board
 ;;
+
+let run_process_two_test color before_player after_player before_board
+  after_board : unit =
+  let player = { empty_player with tokens=before_player } in
+  let board = { empty_board with tokens=before_board } in
+  let action = Action.Two(color) in
+  let (new_player, new_board) = process_action action player board in
+  let new_player_tokens = new_player.tokens in
+  let new_board_tokens = new_board.tokens in
+  let message result =
+    Printf.printf "%s : Player (%s vs %s) Board (%s vs %s)\n"
+    result
+    (verbose_string_of_tokens new_player_tokens)
+    (verbose_string_of_tokens after_player)
+    (verbose_string_of_tokens new_board_tokens)
+    (verbose_string_of_tokens after_board)
+  in
+  match (TokenMap.equal (=) new_player_tokens after_player &&
+  TokenMap.equal (=) new_board_tokens after_board)
+  with
+  | true -> message "passed"
+  | false -> message "FAILED"
+;;
+
+let before_player = (token_map_of []) in
+let after_player = (token_map_of (Normal Red :: Normal Red :: [])) in
+let before_board = after_player in
+let after_board = before_player in
+run_process_two_test Red before_player after_player before_board after_board
+;;
+
+let before_player = (token_map_of []) in
+let after_player = (token_map_of (Normal Red :: Normal Red :: [])) in
+let before_board = (token_map_of (Normal Red :: Normal Red :: Normal Red :: [])) in
+let after_board = (token_map_of (Normal Red :: [])) in
+run_process_two_test Red before_player after_player before_board after_board
+;;
+
+let before_player = (token_map_of []) in
+let after_player = (token_map_of (Normal Red :: Normal Red :: [])) in
+let before_board = (token_map_of (Normal Red :: Normal Red :: Normal Red ::
+  Normal Red :: [])) in
+let after_board = after_player in
+run_process_two_test Red before_player after_player before_board after_board
+;;
