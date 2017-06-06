8c16545e439971f6b7fe2df2d41dbf2e94dd2aa9
Wenley Tong <wenley.tong@gmail.com>
2017-06-06 10:08:21 -0400

    Implement Three case of process action.

--- process.ml
+++ process.ml
@@ -1,3 +1,24 @@
 
+let process_three c1 c2 c3 player board =
+  let new_player =
+    let new_tokens = player.Player.tokens |>
+    Data.TokenCounter.increment (Data.Normal c1) |>
+    Data.TokenCounter.increment (Data.Normal c2) |>
+    Data.TokenCounter.increment (Data.Normal c3)
+    in { player with Player.tokens=new_tokens }
+  in
+  let new_board =
+    let new_tokens = board.Board.tokens |>
+    Data.TokenCounter.decrement (Data.Normal c1) |>
+    Data.TokenCounter.decrement (Data.Normal c2) |>
+    Data.TokenCounter.decrement (Data.Normal c3)
+    in { board with Board.tokens=new_tokens }
+  in
+  (new_player, new_board)
+
 let process_action action player board =
-  (player, board)
+  match action with
+  | Action.Three (c1, c2, c3) ->
+      process_three c1 c2 c3 player board
+  | _ -> (player, board)
+
