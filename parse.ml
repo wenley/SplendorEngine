0e2ffee8518636f17e7a558c6a8710e4c86f0b7c
Wenley Tong <wenley.tong@gmail.com>
2017-06-28 21:52:13 -0700

    Minimal information in cards + nobles.

--- parse.ml
+++ parse.ml
@@ -5,6 +5,10 @@ let num = "\\([0-9]\\)"
 let color = "\\([KBGRW]\\)"
 let token = "\\([KBGRWY]\\)"
 
+let maybe_add key value map =
+  if value > 0 then ColorMap.add key value map
+  else map
+
 let card_for_string (input : string) : card option =
   let pattern = num ^ color ^ num ^ num ^ num ^ num ^ num in
   let regex = Str.regexp pattern in
@@ -24,11 +28,11 @@ let card_for_string (input : string) : card option =
       let red_cost   = int_of_string (Str.matched_group 6 input) in
       let white_cost = int_of_string (Str.matched_group 7 input) in
       let cost =
-        ColorMap.empty |> ColorMap.add Black black_cost
-                       |> ColorMap.add Blue blue_cost
-                       |> ColorMap.add Green green_cost
-                       |> ColorMap.add Red red_cost
-                       |> ColorMap.add White white_cost
+        ColorMap.empty |> maybe_add Black black_cost
+                       |> maybe_add Blue blue_cost
+                       |> maybe_add Green green_cost
+                       |> maybe_add Red red_cost
+                       |> maybe_add White white_cost
       in
       let card = { score; color; cost } in
       Some card
@@ -48,11 +52,11 @@ let noble_for_string (input:string) : noble option =
       let red_cost   = int_of_string (Str.matched_group 5 input) in
       let white_cost = int_of_string (Str.matched_group 6 input) in
       let cost =
-        ColorMap.empty |> ColorMap.add Black black_cost
-                       |> ColorMap.add Blue blue_cost
-                       |> ColorMap.add Green green_cost
-                       |> ColorMap.add Red red_cost
-                       |> ColorMap.add White white_cost
+        ColorMap.empty |> maybe_add Black black_cost
+                       |> maybe_add Blue blue_cost
+                       |> maybe_add Green green_cost
+                       |> maybe_add Red red_cost
+                       |> maybe_add White white_cost
       in
       let noble = { score; cost } in
       Some noble
--- parse.mli
+++ parse.mli
@@ -1,4 +1,3 @@
 
 val card_for_string : string -> Data.card option
-
 val noble_for_string : string -> Data.noble option
